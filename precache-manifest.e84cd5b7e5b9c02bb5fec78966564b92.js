b6964e9041e96a44edfa056a09d9e735faf35447
gibbsben <gibbsben98@gmail.com>
2021-02-09 15:53:56 +0100

    Updates

---
+++ precache-manifest.e84cd5b7e5b9c02bb5fec78966564b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d17f4e088657c29297a8448c5d701c",
+    "url": "/deskweb/index.html"
+  },
+  {
+    "revision": "d07e795929747bcac579",
+    "url": "/deskweb/static/css/main.cc91f1cb.chunk.css"
+  },
+  {
+    "revision": "cc40e76edda751f849e4",
+    "url": "/deskweb/static/js/2.bb074f6e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/deskweb/static/js/2.bb074f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07e795929747bcac579",
+    "url": "/deskweb/static/js/main.f4ba26cd.chunk.js"
+  },
+  {
+    "revision": "1f150a5b2e31e5707fed",
+    "url": "/deskweb/static/js/runtime-main.2db1e762.js"
+  }
+]);
