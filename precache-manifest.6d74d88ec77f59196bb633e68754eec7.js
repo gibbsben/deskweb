bffbf15f00d3c558f445ac8c0ffa06055164e998
gibbsben <gibbsben98@gmail.com>
2021-03-10 18:25:33 +0100

    Updates

---
+++ precache-manifest.6d74d88ec77f59196bb633e68754eec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4634ad8afc944a90b717fb44a7f1c3a0",
+    "url": "/homelessprojectmaastricht.ml/index.html"
+  },
+  {
+    "revision": "07d8b767a76b910145f7",
+    "url": "/homelessprojectmaastricht.ml/static/css/main.157bd086.chunk.css"
+  },
+  {
+    "revision": "cc40e76edda751f849e4",
+    "url": "/homelessprojectmaastricht.ml/static/js/2.bb074f6e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/homelessprojectmaastricht.ml/static/js/2.bb074f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d8b767a76b910145f7",
+    "url": "/homelessprojectmaastricht.ml/static/js/main.782f66fa.chunk.js"
+  },
+  {
+    "revision": "3a736b7df0ba6e566d15",
+    "url": "/homelessprojectmaastricht.ml/static/js/runtime-main.95650490.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.599d2692281bb92c080471b3457dd822.js"
+  "/homelessprojectmaastricht.ml/precache-manifest.6d74d88ec77f59196bb633e68754eec7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homelessprojectmaastricht.ml/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
